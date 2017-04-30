4806265c24b1feb937a0eb3afe3adb0cbaf851aa
Russell Brinkmann <rbb@users.noreply.github.com>
2017-04-30 10:19:56 -0600

    add jpg2mov.sh

---
+++ jpg2mov.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# create an mov from jpegs
+#
+# ffmpeg options culled from the following sources:
+# http://mewiki.project357.com/wiki/X264_Encoding_Suggestions#QuickTime-compatible_Encoding
+# https://trac.ffmpeg.org/wiki/Encode/H.264
+# http://en.wikipedia.org/wiki/High-definition_television
+# http://en.wikibooks.org/wiki/FFMPEG_An_Intermediate_Guide/image_sequence
+mov_name=$(basename "$PWD")
+echo "Creating $mov_name"
+
+# Quicktime output 15 seconds for 86 images
+#time ffmpeg -f image2 -pattern_type glob -i "*.jpg" -r 30 -s 1920x1080 -pix_fmt yuv420p "${mov_name}.mov"
+
+# Prores output 41 seconds for 86 images
+time ffmpeg -f image2 -pattern_type glob -i "*.jpg" -r 30 -s 1920x1080 -c:v prores_ks -pix_fmt yuv444p10le "${mov_name}.mov"
+

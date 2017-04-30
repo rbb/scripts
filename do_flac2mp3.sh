372e713f42da70ab5040cd250a85506c80123350
Russell Brinkmann <rbb@users.noreply.github.com>
2017-04-30 12:07:53 -0600

    add flac to mp3 scripts

---
+++ do_flac2mp3.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# convert all .flac files in current directory to .mp3 files
+
+for f in *.flac
+do
+   #bn=$(basename "$f")
+   bn=${f%.flac}
+   echo $bn
+
+   flac2mp3.sh 6 "$f" "$bn.mp3"
+done

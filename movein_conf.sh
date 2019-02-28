0686b531e83a452ea98efa94c2345fdc32e73eb9
Russell Brinkmann <rbb@users.noreply.github.com>
2019-02-28 14:06:17 -0700

    Change default /etc/ backup location

--- movein_conf.sh
+++ movein_conf.sh
@@ -2,6 +2,6 @@
 
 # This is part of a set of scripts for (re)installing things on a new Linux
 # computer. This file just stores some common variables.
-export movein_etc_src="/home/data_ext/etc_backup"
+export movein_etc_src="/home/data_ext/etc"
 export movein_home_src="/home/data_ext/home/russell"
 

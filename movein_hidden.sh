fed1dfc12e6475252dcc27ff028b5ae4ec6f6bb7
Russell Brinkmann <rbb@users.noreply.github.com>
2019-02-28 13:32:24 -0700

    movein_hidden: add help message, dry run, verbose

--- movein_hidden.sh
+++ movein_hidden.sh
@@ -1,20 +1,55 @@
 #!/bin/bash
 
-# This is a script to assist with setting up a new Linux computer, at FCI. It 
-# attempts to install a bunch of packages, setup networking, setup ssh, and
-# copy some config files from a base user's home directory on a remote 
-# host/server.
+# This is a script to assist with setting up a new Linux computer. It
+# attempts to copy backed up files.
 
 # !!!!!!!!!!!!!!!!! THIS SCRIPT IS NOT TESTED !!!!!!!!!!!!!!!!!!
 # This is just a starter, for next time. - RBB 2013-10-11
 source movein_conf.sh
 
 src="$movein_home_src"
+dst="$HOME"
+
+usage() {
+   echo "Usage: $0 [-s src [-d dest] [-n] [-h]"
+   echo "   default src: $src"
+   echo "   default dest: $dst"
+   exit 1
+}
+
+dryrun=""
+verbose=""
+while getopts ":s:d:hnv" o; do
+    case "${o}" in
+        s)
+            src=${OPTARG}
+            ;;
+        d)
+            dst=${OPTARG}
+            ;;
+        n)
+            dryrun="-n"
+            ;;
+        v)
+            verbose="-v"
+            ;;
+        *)  # Note: this covers -h as well
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
 
 cp_home_target () {
    name=$1
-   if [ ! -e "$HOME/$name" ]; then
-      cp -apr "$src/$name" "$HOME/"
+   if [ ! -e "$dst/$name" ]; then
+      if [ ! -z $verbose ]; then
+         echo "Copying $src/$name to $dst"
+      fi
+      if [ -z $dryrun ]; then
+         cp -apr "$src/$name" "$dst/"
+      fi
    fi
 }
 

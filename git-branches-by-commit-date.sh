6fce0c8166399fbd6c2448cf969c5d4984e5491b
Russell Brinkmann <Russell_Brinkmann@Trimble.com>
2020-09-30 16:11:17 -0600

    Add git-branches-by-commit-date.sh

---
+++ git-branches-by-commit-date.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# https://gist.github.com/jasonrudolph/1810768
+# Credit http://stackoverflow.com/a/2514279
+for branch in `git branch -r | grep -v HEAD`;do echo -e `git show --format="%ci %cr" $branch | head -n 1` \\t$branch; done | sort -r
